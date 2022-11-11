5dcfe3eb4cca4e5fa99729011dcde340f1a71173
shawn@tradev-group.com <shawn@tradev-group.com>
2022-11-11 17:13:06 +0800

    build：静态文件

--- src/main/test.ts
+++ src/main/test.ts
@@ -1,12 +1,13 @@
 import { ipcMain } from "electron"
 import fluentFfmpeg from "fluent-ffmpeg"
-import ffmpegStaticElectron from "ffmpeg-static-electron"
-import ffprobeStaticElectron from "ffprobe-static-electron"
+// import ffmpegStaticElectron from "ffmpeg-static-electron"
+// import ffprobeStaticElectron from "ffprobe-static-electron"
 import getVideoDurationInSeconds from "get-video-duration"
 import { logger } from "./utils/logger"
 import path from "path"
 
 ipcMain.on('ipc-examples', async (event, arg) => {
-  logger.info(arg, ffmpegStaticElectron, ffprobeStaticElectron)
-  logger.info(path.join(process.cwd(), 'ffmpeg', 'ffmpeg.exe'), path.join(process.cwd(), 'ffmpeg', 'ffprobe.exe'))
+  const ffmpegBinary = path.join(__dirname, 'assets', 'ffmpeg.exe');
+  // logger.info(arg, ffmpegStaticElectron, ffprobeStaticElectron)
+  logger.info(ffmpegBinary)
 });
